5f3a365868665f703a69e303024f44403d1eeac3
ruhan85 <ruhanmukadam85@gmail.com>
2025-10-19 21:45:38 +0530

    Add files via upload

---
+++ Smart_Login system.py
@@ -0,0 +1,13 @@
+User_name="Ruhan189"
+Password="Ruhan@ruhan"
+username=input("Enter your username:")
+password=input("Enter your password: ")
+if username==User_name and password==Password:
+    print("Login Successful")
+elif username!=User_name and password==Password:
+    print("Invalid Username")
+elif username==User_name and password!=Password:
+    print("Invalid Password")
+else:
+    print("Invalid Username and Password")
+#I am Ruhan, I have created this smart login system.
